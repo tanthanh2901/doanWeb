f544478ca65ec59ac315a92d9d431e4e79a77c9f
jackson22153 <phucxauxi929@gmail.com>
2024-03-08 00:44:53 +0700

    third commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    //Thông số của CSDL
+    define('DB_HOST', 'localhost:3306');
+    define('DB_NAME', 'db_ct06');
+    define('DB_USER', 'simp');
+    define('DB_PASS', '10122003simp');
+    session_start();
+?>
