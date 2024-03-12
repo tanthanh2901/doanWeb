040d82b7a278d805ff82cc3d3c65dece0eb4c806
Jackson22153 <phucxauxi929@gmail.com>
2024-03-12 15:02:39 +0700

    update

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     //Thông số của CSDL
     define('DB_HOST', 'localhost:3306');
-    define('DB_NAME', 'doanweb');
+    define('DB_NAME', 'db_ct06');
     define('DB_USER', 'simp');
     define('DB_PASS', '10122003simp');
     session_start();
