a44e4bc9f6664c9dfd20c31039a1b3531d16b7fc
Nguyen <NguyenCNTT08@gmail.com>
2024-03-14 10:58:56 +0700

    header-update

--- inc/header.php
+++ inc/header.php
@@ -103,12 +103,23 @@
                   <li class="nav-item">
                     <a class="nav-link" href="blog-single.php">Blog Detail</a>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="contact.php">Contact</a>
-                  </li>
                   <!-- <li class="nav-item">
-                    <a class="nav-link" >Login</a>
+                    <a class="nav-link" href="contact.php">Contact</a>
                   </li> -->
+                  <?php
+                  if(Auth::isLoggedIn()){
+                    echo '<li class="nav-item">'
+                        .' <a class="nav-link" href="logout.php">Logout</a>'.
+                    '</li>';
+                  } else {
+                      echo '<li class="nav-item">'
+                          .' <a class="nav-link" href="login.php">Login</a>'.
+                      '</li>';
+                  }
+                
+                  
+                  ?>
+                  
                 </ul>
               </div>
             </nav>
